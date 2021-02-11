580cee90aa10be0efbdac8cc6bd92c7f75869705
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2021-02-11 17:39:59 +0530

    Update forward_all.py

--- snippets/forward_all.py
+++ snippets/forward_all.py
@@ -15,3 +15,33 @@ async for message in client.search_messages(from_channel, filter="empty"): #You 
             await asyncio.sleep(e.x)
         except:
             pass
+
+#pyrogram v>1.0.7
+#Without Tag
+import asyncio
+from pyrogram.errors import FloodWait
+
+from_channel = -1001312356344 #From Channel ID
+to_channel = -1005382636286 #To Channel ID
+async for message in client.search_messages(from_channel, filter="empty"): #You can change filter value according to your need.
+        try:
+            await message.copy(to_channel) #Without Tag
+            asyncio.sleep(3) #You can Change it according to your need.
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+        except:
+            pass
+#With Tag
+import asyncio
+from pyrogram.errors import FloodWait
+
+from_channel = -1001312356344 #From Channel ID
+to_channel = -1005382636286 #To Channel ID
+async for message in client.search_messages(from_channel, filter="empty"): #You can change filter value according to your need.
+        try:
+            await message.forward(to_channel) #With Tag
+            asyncio.sleep(3) #You can Change it according to your need.
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+        except:
+            pass
